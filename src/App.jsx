261b59862302a938db50eeaa9accabb4d1012b63
KARTIKPAUL <arpilar401@gmail.com>
2025-04-08 17:12:03 +0530

    error resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './Components/LayOut';
+import Layout from './Components/Layout'
 import Home from './Pages/Home';
 import PrivacyPolicy from './Pages/Privacy';
 import TermsAndConditions from './Pages/Terms';
